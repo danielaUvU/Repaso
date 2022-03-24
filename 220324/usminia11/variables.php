fb091c51acea4e972f71c9df80af93401f71d448
danielaUvU <101896519+danielaUvU@users.noreply.github.com>
2022-03-24 17:33:42 -0500

    Add files via upload

---
+++ 220324/usminia11/variables.php
@@ -0,0 +1,11 @@
+<?php 
+// definimos una variable con un texto
+$miVariable = "hola variable";
+// visualizamos la variable en el explorador
+ $miVariable;
+// otro ejemplo variable miValor igual a 2 
+<br>
+$miValor = 2;
+//visualizamos sin embargo realizamos una multiplicacion " 3
+echo 3*$miValor;
+?>
